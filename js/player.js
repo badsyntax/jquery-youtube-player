f3c7cf97f20be997dd4d6728f8481850f91acf7a
Richard Willis <willis.rh@gmail.com>
2010-08-05 20:49:38 +0100

    formatting mostly

--- js/player.js
+++ js/player.js
@@ -80,48 +80,81 @@
 		},
 
 		getPlaylistData : function(callback, error){
+
 			var self = this, playlist = this.options.playlists[this.keys.playlist].videos;
+
 			if (String === playlist.constructor) {
-				self.elements.$playerObject.html('loading playlist..').addClass('playlist-loading');
+
+				self.elements.$playerObject
+					.html('loading playlist..')
+					.addClass('playlist-loading');
+
 				var xhr = $.ajax({
 					type: 'GET',
 					url: self.options.playlistProxy + '?url=' + playlist,
 					dataType: 'json',
 					error: function(){ 
-						error.call(self); 
+						error.call( self ); 
 					},
 					success: function(json){
-						if (!json) { error.call(self); return; }
+
+						if (!json) { 
+							error.call( self ); 
+							return; 
+						}
+
 						// replace playlist url with json array
 						self.options.playlists[self.keys.playlist].videos = [];
+
 						$.each(json.feed.entry, function(key, vid){
+
 							self.options.playlists[self.keys.playlist].videos.push({
 								id: vid.link[0].href.replace(/^[^v]+v.(.{11}).*/, '$1'), // munge video id from href
 								title: vid.title.$t
 							});
 						});
+
 						self.elements.$playerObject.fadeOut(180, function(){
-							callback.call(self);
+
+							callback.call( self );
 						});
 					}
 				});
-			} else callback.call(self);
+
+			} else {
+
+				callback.call( self );
+			}
 		},
 
 		initRouter :  function(){
+
 			var self = this, hash = window.location.hash.replace(/.*?#\//, '');
+
 			this.router = {
 				hash: hash,
 				actions: /\//.test(hash) ? hash.split('/') : ['v'],
 				updateHash: function(){
-					if (self.options.updateHash) 
-					window.location.hash = '/' + self.router.actions[0] + '/' + self.options.playlists[self.keys.playlist].videos[self.keys.video].id;
+
+					if (self.options.updateHash) {
+
+						window.location.hash = 
+							'/' + self.router.actions[0] + 
+							'/' + self.options.playlists[self.keys.playlist].videos[self.keys.video].id;
+					}
 				}
 			};
 			switch(this.router.actions[0]){
-				case 'v' : this.keys.video = this.router.actions[1] ? $.inArray(this.router.actions[1], this.videoIds) : this.keys.video; break;
-				case 'p' : this.keys.video = $.inArray(this.router.actions[1], this.videoIds); this.keys.play = 1; break;
-				default : break;
+				case 'v' : 
+					this.keys.video = 
+						this.router.actions[1] ? $.inArray(this.router.actions[1], this.videoIds) : this.keys.video; 
+					break;
+				case 'p' : 
+					this.keys.video = $.inArray(this.router.actions[1], this.videoIds); 
+					this.keys.play = 1; 
+					break;
+				default : 
+					break;
 			} 
 		},
 		
@@ -181,80 +214,143 @@
 
 		events : {
 			play : function(player, button, playlistItem){
+
 				player.elements.$loader.show();
+
 				player.loadVideo();
+
 				player.ytplayer.playVideo();
 			},
 			pause : function(player, button){
+
 				player.ytplayer.pauseVideo();
 			},
 			prev : function(player, button){
+
 				if (player.keys.video > 0) {
+
 					player.keys.video--;
 					player.elements.toolbar.buttons.play.obj.data('state', 0);
 					player.events.play(player);
 				}
 			},
 			next : function(player, button){
+
 				if (player.keys.video < player.options.playlists[player.keys.playlist].videos.length-1) {
-					if (player.options.shuffle) player.randomVideo();
-					else player.keys.video++;
+
+					if (player.options.shuffle) {
+
+						player.randomVideo();
+					} else {
+
+						player.keys.video++;
+					}
+
 					player.elements.toolbar.buttons.play.obj.data('state', 0);
+
 					player.events.play(player);
 				}
 			},
 			shuffle : function(player, button){ 
+
 				player.randomVideo();
+
 				player.events.play(player);
 			},
 			repeat : function(player, button){
+
 				player.options.repeat = 1;
 			},
 			mute : function(player, button){
-				if (button.obj.data('state')) player.ytplayer.mute();
-				else player.ytplayer.unMute();
+
+				if (button.obj.data('state')) {
+
+					player.ytplayer.mute();
+
+				} else {
+
+					player.ytplayer.unMute();
+				}
 			},
 			playlist : function(player, button){
-				player.elements.$playlistContainer.animate({height: 'toggle', opacity: 'toggle'}, 550);
+
+				player.elements
+					.$playlistContainer
+					.animate({height: 'toggle', opacity: 'toggle'}, 550);
 			},
 			updatePlaylist : function(player){
+
 				player.elements
-				.$playlist.find('li').removeClass('ui-state-active')
-				.each(function(key){
-					if (player.options.playlists[player.keys.playlist].videos[player.keys.video].id == $(this).data('video').id) {
-						var height = $(this).addClass('ui-state-active').outerHeight();
-						player.elements.scrollbar.pos = (key * height) - (3 * height);
-						player.elements.$playlistScroller.scrollTop(player.elements.scrollbar.pos);
-						return false;
-					}
-				});
+					.$playlist
+					.find('li')
+						.removeClass('ui-state-active')
+					.each(function(key){
+
+						if (player.options.playlists[player.keys.playlist].videos[player.keys.video].id == $(this).data('video').id) {
+
+							var height = $(this).addClass('ui-state-active').outerHeight();
+
+							player.elements.scrollbar.pos = (key * height) - (3 * height);
+
+							player.elements.$playlistScroller.scrollTop(player.elements.scrollbar.pos);
+
+							return false;
+						}
+					});
 			},
 			yt : {
 				ready : function(player){
+
 					player.ytplayer = player.elements.$player.find('object:first').get(0);
+
 					player.ytplayer.addEventListener('onStateChange', '_ytplayerevents');
+
 					player.ytplayer.addEventListener('onError', '_ytplayerevents');
+
 					player.cueVideo();
+
 					player.elements.toolbar.$container.fadeIn(800, function(){
+
 						($.isFunction(player.options.onready)) && player.options.onready();
 					});
+
 					player.elements.$playlistsContainer.fadeIn(800);
-					(player.options.showPlaylist) && player.elements.$playlistContainer.animate({height: 'toggle', opacity: 'toggle'}, 550);
-					(player.keys.play) && player.events.play(player)
+
+					if (player.options.showPlaylist) {
+
+						player.elements.$playlistContainer.animate({height: 'toggle', opacity: 'toggle'}, 550);
+					}
+
+					if (player.keys.play) {
+
+						player.events.play(player);
+					}
 				},
 				videoPlay : function(player){
+
 					player.elements.$loader.hide();
+
 					if (!player.elements.toolbar.buttons.play.obj.data('state')) {
+
 						player.events.updatePlaylist(player);
+
 						player.router.updateHash();
+
 						player.elements.toolbar.buttons.play.obj.data('state', 1);
+
 						player.elements.toolbar.updateStates();
+
 						player.elements.$infobar.css({opacity: 0})
+
 						player.updateInfo(320);
 					}
 				},
 				videoEnded : function(player){
-					(player.options.repeat) && player.events.next(player);
+
+					if (player.options.repeat) {
+
+						player.events.next(player);
+					}
 				},
 				error: function(player, state){
 
@@ -278,82 +374,115 @@
 			},
 			scrollbar : {
 				up : function(player, button){
+
 					player.elements.scrollbar.pos = 
 						player.elements.scrollbar.pos > player.elements.$playlist.find('li:first').height() ? 
 						player.elements.scrollbar.pos - player.elements.$playlist.find('li:first').height() : 
 						0;
+
 					player.elements.$playlistScroller.scrollTop(player.elements.scrollbar.pos);
 				},
 				down : function(player, button){
+
 					player.elements.scrollbar.pos = 
 						player.elements.scrollbar.pos < player.elements.$playlist.outerHeight() - player.elements.$playlistScroller.outerHeight() ? 
 						player.elements.scrollbar.pos + player.elements.$playlist.find('li:first').height() : 
 						player.elements.scrollbar.pos;
+
 					player.elements.$playlistScroller.scrollTop(player.elements.scrollbar.pos);
 				}
 			}
 		},
 
 		updateInfo : function(timeout, text){
+
 			var self = this;
+
 			this.elements.$loader.hide();
+
 			if (
 				( this.elements.toolbar.buttons.play.obj.data('state') || this.elements.toolbar.buttons.pause.obj.data('state') ) 
 				&& this.elements.$infobar.css('opacity') < 1
 			) {
 				clearTimeout(this.timer.hideInfo);
+
 				this.timer.showInfo = setTimeout(function(){
+
 					self.elements.$infobar
-					.stop().css({ opacity: 0 }).unbind('click')
-					.html(text || self.options.playlists[self.keys.playlist].videos[self.keys.video].title)
-					.click(function(){ window.open(self.ytplayer.getVideoUrl()); })
-					.animate({ opacity: 1 }, 180, function(){
-						self.timer.hideInfo = setTimeout(function(){
-							self.hideInfo();
-						}, 6000);
-					});
+						.stop()
+						.css({ opacity: 0 })
+						.html(text || self.options.playlists[self.keys.playlist].videos[self.keys.video].title)
+						.unbind('click')
+						.click(function(){ 
+
+							window.open(self.ytplayer.getVideoUrl()); 
+						})
+						.animate({ opacity: 1 }, 180, function(){
+
+							self.timer.hideInfo = setTimeout(function(){
+								self.hideInfo();
+							}, 6000);
+						});
+
 				}, timeout || 0);
 			}
 		},
 
 		hideInfo : function(){
+
 			clearTimeout(this.timer.hideInfo);
+
 			clearTimeout(this.timer.showInfo);
-			this.elements.$infobar.stop().animate({opacity: 0}, 120);
+
+			this.elements.$infobar
+				.stop()
+				.animate({opacity: 0}, 120);
 		},
 
 		cueVideo : function(videoID){
+
 			this.ytplayer.cueVideoById(videoID || this.options.playlists[this.keys.playlist].videos[this.keys.video].id, 0);
 		},
 
 		loadVideo : function(videoID){
+
 			var self = this;
+
 			this.elements.$infobar.stop().css({opacity: 0});
+
 			this.ytplayer.loadVideoById(videoID || this.options.playlists[this.keys.playlist].videos[this.keys.video].id, 0);
+
 			this.router.updateHash();
 		},
 
 		randomVideo : function(){
+
 			this.keys.video = Math.floor(Math.random() * this.options.playlists[this.keys.playlist].videos.length);
+
 			return this.keys.video;
 		},
 
 		createElements : function(){
+
 			return this
-			.createPlayer()
-			.createToolbar()
-			.createInfobar()
-			.createPlaylists()
-			.createTracklist();
+				.createPlayer()
+				.createToolbar()
+				.createInfobar()
+				.createPlaylists()
+				.createTracklist();
 		},
 
 		createPlayer : function(){
+
 			this.elements.$player.width(this.options.width);
+
 			this.elements.$playerVideo.height(this.options.height);
+
 			(this.options.swfobject) && this.options.swfobject.embedSWF(
 				'http://www.youtube.com/apiplayer?enablejsapi=1&version=3&playerapiid=ytplayer&hd=1&showinfo=0', 
 				this.ytplayer.id, this.options.width, this.options.height, '8', null, null, this.options.videoParams
 			);
+
 			return this;
 		},
 
@@ -363,8 +492,11 @@
 			this.elements.toolbar = $.extend({
 				$container: $('<ul id="player-toolbar" class="ui-widget ui-helper-clearfix ui-widget-header ui-corner-all">'),
 				updateStates : function(){
+
 					$.each(self.elements.toolbar.buttons, function(key){
+
 						this.obj.removeClass('ui-state-active');
+
 						(this.obj.data('state')) &&
 						(this.toggle || 
 							(this.toggleButton && 
@@ -375,76 +507,115 @@
 			}, this.options.toolbar || {});
 
 			$.each(this.elements.toolbar.buttons, function(key) {
-				if (this.disabled) { delete self.elements.toolbar.buttons[key]; return true; }
+
+				if (this.disabled) { 
+
+					delete self.elements.toolbar.buttons[key];
+
+					return true; 
+				}
+
 				var buttonObj = this;
+
 				this.obj = 
-				$('<li class="ui-state-default ui-corner-all">')
-				.append('<span class="ui-icon '+this.cssclass+'">')
-				.attr('title', this.text)
-				.data('button', key)
-				.bind('mouseenter', function(){ $(this).toggleClass('ui-state-hover'); })
-				.bind('mouseleave', function(){ $(this).toggleClass('ui-state-hover'); })
-				.click(function(){
-					var button = 
-					$(this)
-					.data('state', $(this).data('state') && buttonObj.toggle ? 0 : 1)
-					.data('button');
+					$('<li class="ui-state-default ui-corner-all">')
+					.append('<span class="ui-icon '+this.cssclass+'">')
+					.attr('title', this.text)
+					.data('button', key)
+					.bind('mouseenter', function(){ $(this).toggleClass('ui-state-hover'); })
+					.bind('mouseleave', function(){ $(this).toggleClass('ui-state-hover'); })
+					.click(function(){
 
-					(buttonObj.toggleButton) &&
-					self.elements.toolbar.buttons[buttonObj.toggleButton].obj.data('state', 0);
+						var button = 
+							$(this)
+							.data('state', $(this).data('state') && buttonObj.toggle ? 0 : 1)
+							.data('button');
+	
+						if (buttonObj.toggleButton) {
 
-					self.elements.toolbar.updateStates();
-					self.events[button](self, buttonObj);
-				})
-				.appendTo(self.elements.toolbar.$container);
+							self.elements.toolbar.buttons[buttonObj.toggleButton].obj.data('state', 0);
+						}
+
+						self.elements.toolbar.updateStates();
+
+						self.events[button](self, buttonObj);
+
+					})
+					.appendTo(self.elements.toolbar.$container);
 			});
 
 			this.elements.$playerVideo.after(this.elements.toolbar.$container);
+
 			this.elements.$loader = $('<span>').addClass('player-loader');
+
 			this.elements.toolbar.$container.after(this.elements.$loader);
+
 			return this;
 		},
 
 		createInfobar : function(){
+
 			this.elements.$infobar = $('<div id="player-infobar">');
+
 			this.elements.$playerVideo.prepend(this.elements.$infobar);
+
 			return this;
 		},
 
 		createPlaylists : function(){
+
 			var self = this;
 			
 			this.elements.$playlistsContainer = 
 				$('<div id="player-playlists">')
 				.height(this.options.height)
 				.append('<div class="player-playlists-heading">');
+
 			this.elements.$playlists = $('<ol>');
 			
 			$.each(this.options.playlists, function(key){
-				$('<li></li>').data('playlist', key).append(this.title).addClass(key === self.keys.playlist ? 'ui-state-active' : '')
-				.click(function(){
-					var li = this;
-					self.keys.playlist = $(this).data('playlist');
-					self.elements.$loader.show();
-					self.getPlaylistData(
-						function(){ // success
-							this.elements.$loader.hide();
-							this.keys.video = this.options.randomStart ? this.randomVideo() : 0;
-							this.elements.toolbar.buttons.play.obj.data('state', 0);
-							this.createTracklist();
-							this.elements.$playlists.find('.ui-state-active').removeClass('ui-state-active');
-							$(li).addClass("ui-state-active");
-							this.cueVideo();
-						},
-						function(){ // error
-							this.elements.$loader.hide();
-							this.elements.$playerObject
-							.html('There was an error loading the playlist.')
-							.removeClass('playlist-loading');
-						}
-					);
-				})
-				.appendTo(self.elements.$playlists);
+
+				$('<li></li>')
+					.data('playlist', key)
+					.append(this.title).addClass(key === self.keys.playlist ? 'ui-state-active' : '')
+					.click(function(){
+
+						var li = this;
+
+						self.keys.playlist = $(this).data('playlist');
+
+						self.elements.$loader.show();
+
+						self.getPlaylistData(
+							function(){ // success
+
+								this.elements.$loader.hide();
+
+								this.keys.video = this.options.randomStart ? this.randomVideo() : 0;
+
+								this.elements.toolbar.buttons.play.obj.data('state', 0);
+
+								this.createTracklist();
+
+								this.elements.$playlists
+									.find('.ui-state-active')
+									.removeClass('ui-state-active');
+
+								$(li).addClass("ui-state-active");
+
+								this.cueVideo();
+							},
+							function(){ // error
+
+								this.elements.$loader.hide();
+
+								this.elements.$playerObject
+									.html('There was an error loading the playlist.')
+									.removeClass('playlist-loading');
+							}
+						);
+					})
+					.appendTo(self.elements.$playlists);
 			});
 
 			this.elements.$player.after(
@@ -457,43 +628,54 @@
 		},
 
 		createTracklist : function(){
+
 			var self = this;
 
 			this.elements.$playlistScroller = this.elements.$playlistScroller || $('<div id="player-playlist-scroller">');
+
 			($.fn.mousewheel) &&
 				this.elements.$playlistScroller.unbind().bind('mousewheel', function(event, delta) {
 					delta > 0 ? self.events.scrollbar.up(self) : self.events.scrollbar.down(self);
 				});
 
 			this.elements.$playlistContainer = this.elements.$playlistContainer || $('<div id="player-playlist-container">');
+
 			this.elements.$playlist = this.elements.$playlist || $('<ol id="player-playlist">');
+
 			this.elements.scrollbar = this.elements.scrollbar || {
 				bar : 
 					$('<div id="player-playlist-scrollbar">')
-					.appendTo(this.elements.$playlistContainer),
+						.appendTo(this.elements.$playlistContainer),
 				up : 
 					$('<span id="player-playlist-scrollbar-up" class="ui-icon ui-icon-circle-triangle-n">')
-					.click(function(){ self.events.scrollbar.up(self); })
-					.appendTo(this.elements.$playlistContainer),
+						.click(function(){ self.events.scrollbar.up(self); })
+						.appendTo(this.elements.$playlistContainer),
 				down : 
 					$('<span id="player-playlist-scrollbar-down" class="ui-icon ui-icon-circle-triangle-s">')
-					.click(function(){ self.events.scrollbar.down(self); })
-					.appendTo(this.elements.$playlistContainer),
+						.click(function(){ self.events.scrollbar.down(self); })
+						.appendTo(this.elements.$playlistContainer),
 				pos : 0
 			}
 
 			this.elements.$playlist.empty();
+
 			this.videoIds = [];
+
 			$.each(this.options.playlists[this.keys.playlist].videos, function(){
+
 				self.videoIds.push(this.id);
-				$('<li></li>').data('video', this).append(this.title).unbind()
-				.click(function(){
-					self.keys.video = $.inArray($(this).data('video').id, self.videoIds);
-					self.elements.toolbar.buttons.play.obj.data('state', 0);
-					self.events.updatePlaylist(self);
-					self.events.play(self, null, this);
-				})
-				.appendTo(self.elements.$playlist);
+
+				$('<li></li>')
+					.data('video', this)
+					.append(this.title)
+					.unbind()
+					.click(function(){
+						self.keys.video = $.inArray($(this).data('video').id, self.videoIds);
+						self.elements.toolbar.buttons.play.obj.data('state', 0);
+						self.events.updatePlaylist(self);
+						self.events.play(self, null, this);
+					})
+					.appendTo(self.elements.$playlist);
 			});
 
 			this.elements.$playerVideo.after(
